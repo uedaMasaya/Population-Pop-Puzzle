fd430a880de7fb0b056a7005f629ab8d9c568519
uedaMasaya <93070798+uedaMasaya@users.noreply.github.com>
2024-08-31 23:01:33 +0900

    ヘッダーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
   end
   
   root 'home#index'
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'contacts#new'
   
   resources :contacts, only: [:new, :create]
+  resource :profile, only: :show, controller: 'users'
 
   resources :questions, only: [:index, :show, :result] do
     collection do
