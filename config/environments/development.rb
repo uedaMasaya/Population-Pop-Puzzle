508e6ff1c55acaca49d8ba654760676da1c4ba0e
uedaMasaya <93070798+uedaMasaya@users.noreply.github.com>
2024-09-12 01:02:31 +0900

    デプロイURLの許可２

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << "population-pop-puzzle.onrender.com"
 end
